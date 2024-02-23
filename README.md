e51d738d9725312cde8d8e578d368ee93cb46dd1
sabarisharan <sabarisharan.surendra@teknopoint.in>
2024-02-23 19:54:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# WebRTC QR Signaling Channel
+
+[**LIVE**](https://tomashubelbauer.github.io/webrtc-qr-signaling-channel)
+
+- Start the application using `npx https-localhost`
+- Find out what the network URL of your laptop is (check `npx serve .` to find it)
+- Open the application on your laptop at the network URL
+- Allow access to the webcam on your laptop (used to scan the code on the phone)
+- Wait for the QR code to load and scan it using your phone's built in QR scanner
+- Allow access to the webcam on your phone (not used, required for WebRTC ICE)
+- Wait for the QR code on your phone to load and show it to your laptop webcam
+- Watch both codes disappear as the connection is established between the devices
+
+On iOS, the Camera app is capable of scanning QR codes by default and you can
+also pin a control to the Control Centre called *Scan QR Code* which opens it.
+
+On Android, the Camera app scans QR codes if the *Google Lens Suggestion* option
+is enabled in the Camera app settings.
+
+## To-Do
+
+### Handle more states: user media handlers, peer connection handlers…
+
+### Figure out why *ICE failed, add a TURN server and see about:webrtc for more details* happens
+
+Even though I am able to successfully establish the connection.
+
+### Add UI which shows the states of the peer connection and the data channel
+
+### Demonstrate the functionality by implementing a local storage syncing mechanism
+
+On top of the data channel peer connection. Send only diffs instead of the full
+data with each change and on the other end patch the existing content with the
+change. Also probably add version number and ensure the current messags one is
+one larger than the last one so we didn't miss messages. If it is the same we
+have a conflict and need to present resolution UI.
+
+### Parse out important bits from the SDP either way and display only that
+
+In the codes so that they are easier to scan on both ends.
+
+### Add a UI button for joining without using the phone QR scanner flow
+
+In case of connecting two laptops/phones or not having a QR scanner on the phone. This will
+just need to add another bit of data to the code - if it is another offer or an
+answer and based on it switch the flows. (Because by default both would show an
+offer and the one which notices each other first should display an answer for
+the other to then notice.)
+
+### Add a read-receipt style message to acknowledge reception of a message on the other side
+
+### Research options of keeping the application alive while the device gets locked
+
+(maybe using a service worker? maybe there is a JS API for this?) and/or how to
+reconnect a peer connection that got interrupted by the lock (can reuse the same
+SDP?).
+
+### Research fountation codes for more efficient message transfer
---
+++ index.js
@@ -0,0 +1,111 @@
+window.addEventListener('load', async () => {
+  const streamVideo = document.getElementById('streamVideo');
+  const codeSvg = document.getElementById('codeSvg');
+  const shareTextArea = document.getElementById('shareTextArea');
+
+  const peerConnection = new RTCPeerConnection({ iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] });
+  if (location.search) {
+    /* Answer */
+
+    // This is needed by Safari to gather candidates even for data-channel-only peer connections :-( https://stackoverflow.com/a/53914556/2715716
+    await navigator.mediaDevices.getUserMedia({ video: true });
+    const sdp = atob(location.search.substring('?'.length));
+    await peerConnection.setRemoteDescription({ sdp, type: 'offer' });
+    await peerConnection.setLocalDescription(await peerConnection.createAnswer());
+    peerConnection.addEventListener('icecandidate', event => render(peerConnection.localDescription.sdp, event.candidate === null));
+    peerConnection.addEventListener('datachannel', event => {
+      streamVideo.remove();
+      codeSvg.remove();
+      event.channel.addEventListener('open', () => {
+        document.body.append(document.createTextNode('Answerer connected. '));
+        event.channel.send('hi, this is answerer');
+
+        shareTextArea.disabled = false;
+        shareTextArea.addEventListener('input', () => event.channel.send(shareTextArea.value));
+      });
+
+      event.channel.addEventListener('message', event => shareTextArea.value = event.data);
+      event.channel.addEventListener('close', () => document.body.append(document.createTextNode('Answerer disconnected. ')));
+      event.channel.addEventListener('error', () => document.body.append(document.createTextNode('Answerer errored. ')));
+    });
+  } else {
+    /* Offer */
+
+    const dataChannel = peerConnection.createDataChannel('webrtc-qr-signaling-channel');
+    await peerConnection.setLocalDescription(await peerConnection.createOffer());
+    peerConnection.addEventListener('icecandidate', async event => {
+      const final = event.candidate === null;
+      render(location.origin + location.pathname + '?' + btoa(peerConnection.localDescription.sdp), final);
+      if (final) {
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+        streamVideo.srcObject = mediaStream;
+        await streamVideo.play();
+        const frameCanvas = document.createElement('canvas');
+        frameCanvas.width = streamVideo.videoWidth;
+        frameCanvas.height = streamVideo.videoHeight;
+        const context = frameCanvas.getContext('2d');
+        void async function scan() {
+          context.drawImage(streamVideo, 0, 0);
+          const { data, width, height } = context.getImageData(0, 0, frameCanvas.width, frameCanvas.height);
+          const code = jsQR(data, width, height);
+          if (code && code.data) {
+            mediaStream.getTracks().forEach(track => track.stop());
+            streamVideo.remove();
+            codeSvg.remove();
+
+            const sdp = code.data;
+            await peerConnection.setRemoteDescription({ sdp, type: 'answer' });
+            return;
+          }
+
+          window.requestAnimationFrame(scan);
+        }()
+      }
+    });
+
+    dataChannel.addEventListener('open', () => {
+      document.body.append(document.createTextNode('Offerer connected. '));
+      dataChannel.send('hi, this is offerer');
+
+      shareTextArea.disabled = false;
+      shareTextArea.addEventListener('input', () => dataChannel.send(shareTextArea.value));
+    });
+
+    dataChannel.addEventListener('message', event => shareTextArea.value = event.data);
+    dataChannel.addEventListener('close', () => document.body.append(document.createTextNode('Offerer disconnected. ')));
+    dataChannel.addEventListener('error', () => document.body.append(document.createTextNode('Offerer errored. ')));
+  }
+
+  function render(code, final) {
+    const qr = qrcode(0, 'L');
+    qr.addData(code, 'Byte');
+    qr.make();
+
+    const moduleCount = qr.getModuleCount();
+    const fragment = document.createDocumentFragment();
+    for (let y = 0; y < moduleCount; y++) {
+      for (let x = 0; x < moduleCount; x++) {
+        if (!qr.isDark(x, y)) {
+          continue;
+        }
+
+        // Set attribute must be used with SVG namespace tags
+        const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttribute('x', x);
+        rect.setAttribute('y', y);
+        rect.setAttribute('width', 1);
+        rect.setAttribute('height', 1);
+        fragment.append(rect);
+      }
+    }
+
+    // Set attribute must be used with SVG namespace tags
+    codeSvg.setAttribute('width', moduleCount);
+    codeSvg.setAttribute('height', moduleCount);
+    codeSvg.setAttribute('viewBox', `0 0 ${moduleCount} ${moduleCount}`);
+    codeSvg.setAttribute('class', final ? 'final' : '');
+
+    codeSvg.innerHTML = '';
+    codeSvg.append(fragment);
+  }
+});
